953703996bfb3857e0f8c95c3c84bbbaa0f00cd4
Gh0st116 <66926881+Gh0st116@users.noreply.github.com>
2022-07-26 20:29:40 -0300

    Upload

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+    namespace Reactnavigation {
+        interface RootParamList {
+            home: undefined;
+            new: undefined;
+            details: { orderId: string };
+        }
+    }
+}
